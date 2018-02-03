0dd1d03c3e655eeae29ee9d73403e3648760f699
mugagambi <mugagambi@gmail.com>
2018-02-03 11:18:19 +0300

    added order products

--- core/models.py
+++ core/models.py
@@ -13,6 +13,7 @@ class User(AbstractUser):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
--- sales/models.py
+++ sales/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from core.models import User
+from core.models import User, Product
 
 
 # Create your models here.
@@ -22,3 +22,31 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    received_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    date_delivery = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.customer)
+
+
+class OrderProducts(models.Model):
+    GRADES = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4)
+    )
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    grade = models.PositiveSmallIntegerField(choices=GRADES)
+    qty = models.DecimalField(decimal_places=2, max_digits=8)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return str(self.order)
