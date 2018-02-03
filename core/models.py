8b2405e502a085d525e191146e30f316cdb203a4
mugagambi <mugagambi@gmail.com>
2018-02-03 10:54:42 +0300

    added products and customers

--- core/models.py
+++ core/models.py
@@ -5,3 +5,14 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     phone_number = models.PositiveIntegerField()
+    is_sales_agent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- sales/models.py
+++ sales/models.py
@@ -1,9 +1,24 @@
 from django.db import models
+from core.models import User
 
 
 # Create your models here.
 class Region(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    country_code = models.PositiveSmallIntegerField()
+    phone_number = models.PositiveIntegerField()
+    added_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
