f7324fac6a4ca77431e1b7106e2ebea4f3013969
mugagambi <mugagambi@gmail.com>
2018-02-05 16:56:50 +0300

    added aggregation view documentation

--- core/views.py
+++ core/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from core import models
 from core import serializers
@@ -6,5 +5,17 @@ from core import serializers
 
 # Create your views here.
 class AggregationCenterViewSet(viewsets.ModelViewSet):
+    """
+       retrieve:
+       Return the given aggregation center.
+
+       list:
+       Return a list of all the existing aggregation centers.
+
+       create:
+       Create a new aggregation center instance.
+       update:
+       Update the given aggregation center
+       """
     queryset = models.AggregationCenter.objects.all()
     serializer_class = serializers.AggregationCenterSerializer
