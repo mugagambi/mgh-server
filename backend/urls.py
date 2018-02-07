23a0a67880368d50c62ede6ea4191516f4ecfd17
mugagambi <mugagambi@gmail.com>
2018-02-07 14:00:21 +0300

    basic template set up

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='base.html')),
     path('admin/', admin.site.urls),
     path('api/core/', include('core.urls')),
     path('api/sales/', include('sales.urls')),
