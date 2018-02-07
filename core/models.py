4996571d67a5a3388b47d5b7635a153bf64363fa
mugagambi <mugagambi@gmail.com>
2018-02-07 14:36:50 +0300

    removed is sales agent

--- core/models.py
+++ core/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     phone_number = models.PositiveIntegerField(null=True)
-    is_sales_agent = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
