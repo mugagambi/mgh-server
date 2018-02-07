924a0b564ed1d07963effd8e3364bb791c696ff6
mugagambi <mugagambi@gmail.com>
2018-02-08 00:27:22 +0300

    can add groups

--- core/web_urls.py
+++ core/web_urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 app_name = 'core'
 urlpatterns = [
-    path('users/groups', web_views.GroupListView.as_view(), name='groups')
+    path('users/groups', web_views.GroupListView.as_view(), name='groups'),
+    path('users/groups/add', web_views.GroupCreateView.as_view(), name='group-create'),
+    path('users/groups/<int:pk>/', web_views.GroupCreateView.as_view(), name='group-edit')
 ]
--- core/web_views.py
+++ core/web_views.py
@@ -1,9 +1,28 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from core import models
 from django.contrib.auth.models import Group, Permission
 from django.contrib.auth.mixins import LoginRequiredMixin
+from core import forms
 
 
 class GroupListView(LoginRequiredMixin, ListView):
     model = Group
     template_name = 'core/group_list.html'
+
+
+class GroupCreateView(CreateView):
+    model = Group
+    template_name = 'core/group_create_form.html'
+    success_url = '/users/groups'
+    form_class = forms.GroupForm
+
+    def get_context_data(self, **kwargs):
+        permissions = Permission.objects.all()
+        kwargs['permissions'] = permissions
+        return super(GroupCreateView, self).get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        object = form.save()
+        object.permissions.set(form.cleaned_data.get('permissions'))
+        return super(GroupCreateView, self).form_valid(form)
