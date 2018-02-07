988af8c5cdce40f7b7e6051237f1e2b0d4ca7e08
mugagambi <mugagambi@gmail.com>
2018-02-07 23:16:33 +0300

    added ids

--- core/serializers.py
+++ core/serializers.py
@@ -5,34 +5,34 @@ from core import models
 class AggregationCenterSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.AggregationCenter
-        fields = ('name', 'location')
+        fields = ('id', 'name', 'location')
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Product
-        fields = ('name',)
+        fields = ('id', 'name',)
 
 
 class AggregationCenterProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.AggregationCenterProduct
-        fields = ('aggregation_center', 'product', 'active')
+        fields = ('id', 'aggregation_center', 'product', 'active')
 
 
 class CrateTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CrateType
-        fields = ('name', 'weight')
+        fields = ('id', 'name', 'weight')
 
 
 class CrateSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Crate
-        fields = ('number', 'type')
+        fields = ('id', 'number', 'type')
 
 
 class GradeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Grade
-        fields = ('name',)
+        fields = ('id', 'name',)
