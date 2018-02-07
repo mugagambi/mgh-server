b4f2336e8375176c74155afbb86a4311a695828d
mugagambi <mugagambi@gmail.com>
2018-02-08 01:06:42 +0300

    can update

--- core/web_urls.py
+++ core/web_urls.py
@@ -5,5 +5,5 @@ app_name = 'core'
 urlpatterns = [
     path('users/groups', web_views.GroupListView.as_view(), name='groups'),
     path('users/groups/add', web_views.GroupCreateView.as_view(), name='group-create'),
-    path('users/groups/<int:pk>/', web_views.GroupCreateView.as_view(), name='group-edit')
+    path('users/groups/<int:pk>/', web_views.GroupUpdateView.as_view(), name='group-edit')
 ]
--- core/web_views.py
+++ core/web_views.py
@@ -1,5 +1,5 @@
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from core import models
 from django.contrib.auth.models import Group, Permission
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -26,3 +26,23 @@ class GroupCreateView(CreateView):
         object = form.save()
         object.permissions.set(form.cleaned_data.get('permissions'))
         return super(GroupCreateView, self).form_valid(form)
+
+
+class GroupUpdateView(UpdateView):
+    model = Group
+    template_name = 'core/group_update_form.html'
+    success_url = '/users/groups'
+    form_class = forms.GroupForm
+
+    def get_context_data(self, **kwargs):
+        permissions = Permission.objects.all()
+        current_permissions = self.object.permissions.all()
+        kwargs['permissions'] = permissions
+        kwargs['current_permissions'] = current_permissions
+        return super(GroupUpdateView, self).get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        object = form.save()
+        object.permissions.clear()
+        object.permissions.set(form.cleaned_data.get('permissions'))
+        return super(GroupUpdateView, self).form_valid(form)
