6c8a41cd20850df4cc169c59934c65cf01bbb82f
mugagambi <mugagambi@gmail.com>
2018-03-16 00:56:49 +0300

    list regions

--- sales/web_urls.py
+++ sales/web_urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from sales import web_views
 
 urlpatterns = [
-    path('orders/', web_views.OrdersView.as_view())
+    path('orders/', web_views.OrdersView.as_view()),
+    path('regions/', web_views.RegionList.as_view(), name='regions')
 ]
--- sales/web_views.py
+++ sales/web_views.py
@@ -1,5 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from sales import models
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class OrdersView(TemplateView):
     template_name = 'sales/orders.html'
+
+
+class RegionList(LoginRequiredMixin, ListView):
+    model = models.Region
+    template_name = 'sales/regions/index.html'
