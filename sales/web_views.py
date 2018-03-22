fce8b455acb3ab231340d85715cfb339db2cb21a
mugagambi <mugagambi@gmail.com>
2018-03-22 22:53:53 +0300

    order customer details

--- sales/web_views.py
+++ sales/web_views.py
@@ -242,12 +242,12 @@ def update_order(request, pk):
             form.fields['price'].queryset = models.CustomerPrice.objects.filter(customer=order.customer)
             form.fields['discount'].queryset = models.CustomerDiscount.objects.filter(customer=order.customer)
     return render(request, 'crud/formset-create.html', {'formset': formset,
-                                                        'create_name': 'Update order ' + order.number})
+                                                        'create_name': 'Update order ' + order.number + ' for ' + order.customer.name})
 
 
 class DeleteOrder(DeleteView):
     def post(self, request, *args, **kwargs):
-        messages.success(request, 'Customer order successfully!')
+        messages.success(request, 'Order removed!')
         return super().post(request, *args, **kwargs)
 
     template_name = 'crud/delete.html'
