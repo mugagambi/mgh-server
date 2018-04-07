11e8e2efcfd6c6f04f42df724a2c97fe86e734fb
mugagambi <mugagambi@gmail.com>
2018-04-07 21:18:20 +0300

    removed header

--- reports/views.py
+++ reports/views.py
@@ -38,7 +38,7 @@ def cash_sale_summary_report(request, date_0, date_1):
     else:
         customer_total_amount_cash = {'total_amount': 0}
     customer_receipts = customer_report.values(
-        'receipt__number').annotate(total_amount=Sum('amount'))
+        'receipt__number').annotate(total_amount=Sum('amount')).order_by('total_amount')
     cash_report = models.CashReceiptParticular.objects.filter(cash_receipt__date__range=(date_0, date_1))
     if cash_report.exists():
         cash_total_amount_cash = cash_report.aggregate(total_amount=Sum(F('qty') * F('price')))
