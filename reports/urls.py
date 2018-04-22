c99fecf23833e8d56330496c25971a2c06294d61
mugagambi <mugagambi@gmail.com>
2018-04-22 03:44:09 +0300

    outward stock

--- reports/urls.py
+++ reports/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('index/', views.index, name='report-index'),
     path('sales/', views.sale_summary_report, name='sales-summary'),
     path('sales/cash/<str:date_0>/<str:date_1>/', views.cash_sale_summary_report, name='cash-sales-summary'),
-    path('sales/outward_products/period/', outward_products.outward_product_summary_period,
+    path('sales/outward_product/period/', outward_products.outward_product_summary_period,
          name='outward_product_period'),
-    path('sales/outward_products/<str:date_0>/<str:date_1>/', outward_products.outward_product_summary_report,
+    path('sales/outward_product/<str:date_0>/<str:date_1>/', outward_products.outward_product_summary_report,
          name='outward_product_report')
 ]
