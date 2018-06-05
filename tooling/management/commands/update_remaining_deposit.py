388a74fae91e5a0812e81268702d76d5ccd0c925
mugagambi <mugagambi@gmail.com>
2018-06-05 12:15:57 +0300

    update remaining deposit command works

---
+++ tooling/management/commands/update_remaining_deposit.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import F
+
+from sales.models import CustomerDeposit
+from tooling.models import Commands
+
+
+class Command(BaseCommand):
+    command = 'update_remaining_deposit'
+    help = 'Help update the remaining deposit amount in a system that has already gone to production.Runs once'
+
+    def handle(self, *args, **options):
+        command, created = Commands.objects.get_or_create(command=self.command,
+                                                          defaults={
+                                                              'runs': 'O'
+                                                          })
+        if not created and command.run_times > 1:
+            raise CommandError('This command can\'t be ran more than once')
+        else:
+            try:
+                CustomerDeposit.objects.all().update(remaining_amount=F('amount'))
+                command.run_times = command.run_times + 1
+                command.save()
+                self.stdout.write(self.style.SUCCESS('Deposits have been updated successfully'))
+            except Exception:
+                raise CommandError('An error occurred when updating deposits.The error %s', Exception)
--- tooling/models.py
+++ tooling/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Commands(models.Model):
+    RUNS = (
+        ('O', 'Once'),
+        ('M', 'Multiple')
+    )
+    command = models.CharField(max_length=50, unique=True)
+    runs = models.CharField(max_length=1, choices=RUNS)
+    run_times = models.PositiveSmallIntegerField(default=0)
+    last_run = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.command
