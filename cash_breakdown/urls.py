5e8d33dedbeb320852c70c603f586d384c2d0956
mugagambi <mugagambi@gmail.com>
2018-06-19 20:07:07 +0300

    bank names ``

--- cash_breakdown/urls.py
+++ cash_breakdown/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('banks/', views.BankList.as_view(), name='banks'),
-    path('banks/create/', views.create_banks, name='create_banks')
+    path('banks/create/', views.create_banks, name='create_banks'),
+    path('banks/<int:pk>/update/', views.UpdateBank.as_view(), name='update_bank'),
+    path('banks/<int:pk>/delete/', views.DeleteBank.as_view(), name='delete_bank'),
 ]
--- cash_breakdown/views.py
+++ cash_breakdown/views.py
@@ -1,11 +1,12 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, permission_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 # Create your views here.
+from django.contrib.messages.views import SuccessMessageMixin
 from django.forms import modelformset_factory
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView, DeleteView
 
 from cash_breakdown.models import Bank
 
@@ -32,3 +33,25 @@ def create_banks(request):
     else:
         formset = bank_formset(queryset=Bank.objects.none())
     return render(request, 'cash_breakdown/create_banks.html', {'formset': formset})
+
+
+class UpdateBank(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Bank
+    fields = ('name',)
+    permission_required = 'core.change_bank'
+    success_message = 'Bank details have been updated'
+    success_url = reverse_lazy('banks')
+    template_name = 'cash_breakdown/update_bank.html'
+
+
+class DeleteBank(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        messages.success(request, 'Bank removed successfully')
+        return super(DeleteBank, self).delete(request, *args, **kwargs)
+
+    template_name = 'crud/delete.html'
+    model = Bank
+    success_url = reverse_lazy('banks')
+    permission_required = 'bank.delete_bank'
