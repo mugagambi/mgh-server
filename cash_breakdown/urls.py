4908f0b1c64833d391977a496309c1f4f55e3c81
mugagambi <mugagambi@gmail.com>
2018-06-19 18:55:47 +0300

    create banks

--- cash_breakdown/urls.py
+++ cash_breakdown/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('banks/', views.BankList.as_view(), name='banks')
+    path('banks/', views.BankList.as_view(), name='banks'),
+    path('banks/create/', views.create_banks, name='create_banks')
 ]
--- cash_breakdown/views.py
+++ cash_breakdown/views.py
@@ -1,5 +1,10 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+from django.forms import modelformset_factory
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views.generic import ListView
 
 from cash_breakdown.models import Bank
@@ -7,3 +12,23 @@ from cash_breakdown.models import Bank
 
 class BankList(LoginRequiredMixin, ListView):
     model = Bank
+
+
+@login_required()
+@permission_required('core.add_bank', raise_exception=True)
+def create_banks(request):
+    """
+        Add  banks
+        :param request:
+        :return response:
+        """
+    bank_formset = modelformset_factory(Bank, fields=('name',), extra=0, min_num=1)
+    if request.method == 'POST':
+        formset = bank_formset(request.POST)
+        if formset.is_valid():
+            formset.save()
+            messages.success(request, 'banks added successfully!')
+            return redirect(reverse_lazy('banks'))
+    else:
+        formset = bank_formset(queryset=Bank.objects.none())
+    return render(request, 'cash_breakdown/create_banks.html', {'formset': formset})
