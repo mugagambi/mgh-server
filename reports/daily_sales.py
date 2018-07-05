461adda39404da286bb11bf49687f7901ee81421
mugagambi <mugagambi@gmail.com>
2018-07-05 17:22:18 +0300

    using chartjs

--- reports/daily_sales.py
+++ reports/daily_sales.py
@@ -6,9 +6,12 @@ from django.db.models import DateField, Sum, Min, Max
 from django.db.models.functions import Trunc
 from django.shortcuts import redirect, render
 from django.utils import timezone
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from reports import forms
 from sales import models
+from .serializers import DailySalesChartSerializer
 
 
 @login_required()
@@ -28,6 +31,9 @@ def period(request):
 
 
 def report(request, date_0, date_1):
+    period = get_date_period_in_range(date_0, date_1)
+    date_0_str = date_0
+    date_1_str = date_1
     date_0 = timezone.datetime.strptime(date_0, '%Y-%m-%d').date()
     date_1 = timezone.datetime.strptime(date_1, '%Y-%m-%d').date()
     date_0_datetime = timezone.datetime.combine(date_0, datetime.time(0, 0))
@@ -36,19 +42,6 @@ def report(request, date_0, date_1):
         filter(receipt__date__range=(date_0_datetime, date_1_datetime)). \
         annotate(day=Trunc('receipt__date', 'day', output_field=DateField(), )). \
         values('day').annotate(total=Sum('total')).order_by('day')
-    sales_summary_range = sales.aggregate(
-        low=Min('total'),
-        high=Max('total'),
-    )
-    high = sales_summary_range.get('high', 0)
-    low = sales_summary_range.get('low', 0)
-    sales_summary_over_time = [{
-        'period': x['day'],
-        'total': x['total'] or 0,
-        'pct':
-            ((x['total'] or 0) - low) / (high - low) * 100
-            if high > low else 0,
-    } for x in sales]
     cash_sales = models.CashReceiptParticular.objects. \
         filter(cash_receipt__date__range=(date_0_datetime, date_1_datetime)). \
         annotate(day=Trunc('cash_receipt__date', 'day', output_field=DateField(), )). \
@@ -88,6 +81,58 @@ def report(request, date_0, date_1):
         cash_sales = paginator.page(paginator.num_pages)
     context_data = {'sales': sales, 'date_0': date_0_datetime, 'date_1': date_1_datetime, 'cash_sales': cash_sales,
                     'total_sales': total_sales, 'total_cash_sales': total_cash_sales,
-                    'sales_summary_over_time': sales_summary_over_time,
-                    'cash_sales_summary_over_time': cash_sales_summary_over_time}
+                    'cash_sales_summary_over_time': cash_sales_summary_over_time,
+                    'date_0_str': date_0_str, 'date_1_str': date_1_str,
+                    'period': period}
     return render(request, 'reports/daily_sales/report.html', context_data)
+
+
+def get_date_period_in_range(date_0, date_1):
+    date_0 = timezone.datetime.strptime(date_0, '%Y-%m-%d').date()
+    date_1 = timezone.datetime.strptime(date_1, '%Y-%m-%d').date()
+    delta = date_1 - date_0
+    if delta.days == 30:
+        return 'hour'
+    if 32 > delta.days > 1:
+        return 'day'
+    if 365 > delta.days > 33:
+        return 'month'
+    return 'month'
+
+
+@api_view(['GET'])
+def get_json_response(request, date_0, date_1):
+    period = get_date_period_in_range(date_0, date_1)
+    date_0 = timezone.datetime.strptime(date_0, '%Y-%m-%d').date()
+    date_1 = timezone.datetime.strptime(date_1, '%Y-%m-%d').date()
+    date_0_datetime = timezone.datetime.combine(date_0, datetime.time(0, 0))
+    date_1_datetime = timezone.datetime.combine(date_1, datetime.time(23, 59))
+    sales = models.ReceiptParticular.objects. \
+        filter(receipt__date__range=(date_0_datetime, date_1_datetime)). \
+        annotate(day=Trunc('receipt__date', period, output_field=DateField(), )). \
+        values('day').annotate(total=Sum('total')).order_by('day')
+    sales_summary_over_time = [{
+        'period': x['day'],
+        'total': x['total'] or 0,
+    } for x in sales]
+    serializer = DailySalesChartSerializer(sales_summary_over_time, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_cash_sale_json_response(request, date_0, date_1):
+    period = get_date_period_in_range(date_0, date_1)
+    date_0 = timezone.datetime.strptime(date_0, '%Y-%m-%d').date()
+    date_1 = timezone.datetime.strptime(date_1, '%Y-%m-%d').date()
+    date_0_datetime = timezone.datetime.combine(date_0, datetime.time(0, 0))
+    date_1_datetime = timezone.datetime.combine(date_1, datetime.time(23, 59))
+    sales = models.CashReceiptParticular.objects. \
+        filter(cash_receipt__date__range=(date_0_datetime, date_1_datetime)). \
+        annotate(day=Trunc('cash_receipt__date', period, output_field=DateField(), )). \
+        values('day').annotate(total=Sum('total')).order_by('day')
+    sales_summary_over_time = [{
+        'period': x['day'],
+        'total': x['total'] or 0,
+    } for x in sales]
+    serializer = DailySalesChartSerializer(sales_summary_over_time, many=True)
+    return Response(serializer.data)
---
+++ reports/serializers.py
@@ -0,0 +1,17 @@
+from django.utils.formats import number_format
+from rest_framework import serializers
+
+
+class DailySalesChartSerializer(serializers.Serializer):
+    period = serializers.DateField(read_only=True)
+    total = serializers.SerializerMethodField('total_number', read_only=True)
+
+    def total_number(self, obj):
+        print(obj['total'])
+        return number_format(obj['total'])
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
