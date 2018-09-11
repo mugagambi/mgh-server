0862b05fb6d116b92fd95f5568643dbb8c45b9a6
mugagambi <mugagambi@gmail.com>
2018-09-11 13:28:38 +0300

    seed payments

--- sales/management/commands/seed_customer_accounts.py
+++ sales/management/commands/seed_customer_accounts.py
@@ -10,20 +10,20 @@ class Command(BaseCommand):
         items = []
         i = 1
         j = 0
-        for instance in ReceiptParticular.objects.all():
-            j += 1
-            i += 1
-            items.append(CustomerAccount(number=str(j),
-                                         customer=instance.receipt.customer,
-                                         amount=-instance.total,
-                                         date=instance.receipt.date,
-                                         type='P',
-                                         receipt=instance.receipt))
-            if i == 10000:
-                CustomerAccount.objects.bulk_create(items)
-                print(f'writing {instance.id}')
-                i = 0
-                items = []
+        # for instance in ReceiptParticular.objects.all():
+        #     j += 1
+        #     i += 1
+        #     items.append(CustomerAccount(number=str(j),
+        #                                  customer=instance.receipt.customer,
+        #                                  amount=-instance.total,
+        #                                  date=instance.receipt.date,
+        #                                  type='P',
+        #                                  receipt=instance.receipt))
+        #     if i == 10000:
+        #         CustomerAccount.objects.bulk_create(items)
+        #         print(f'writing {instance.id}')
+        #         i = 0
+        #         items = []
         for instance in ReceiptPayment.objects.all():
             if instance.type == 1:
                 type = 'Q'
