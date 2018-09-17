5c7384e2946de8d9ef3a59c6a80a98263234414e
mugagambi <mugagambi@gmail.com>
2018-09-17 19:27:17 +0300

    remove admins

--- admins/urls.py
+++ admins/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.AdminsList.as_view(), name='admin_list'),
-    path('add/', views.CreateAdmin.as_view(), name='add_admin')
+    path('add/', views.CreateAdmin.as_view(), name='add_admin'),
+    path('add/<int:pk>/update/', views.UpdateAdmin.as_view(), name='change_admin'),
+    path('add/<int:pk>/delete/', views.remove_admin, name='remove_admin')
 ]
--- admins/views.py
+++ admins/views.py
@@ -1,8 +1,11 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import (AccessMixin, LoginRequiredMixin,
                                         PermissionRequiredMixin)
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView
 
 from .models import Admin
 
@@ -23,3 +26,24 @@ class CreateAdmin(LoginRequiredMixin, PermissionRequiredMixin, AccessMixin, Succ
     success_url = reverse_lazy('admin_list')
     success_message = 'Admin added successfully'
     template_name = 'admins/create.html'
+
+
+class UpdateAdmin(LoginRequiredMixin, PermissionRequiredMixin, AccessMixin, SuccessMessageMixin, UpdateView):
+    permission_denied_message = 'You don\'t have access to this page.'
+    permission_required = 'admins.change_admin'
+    raise_exception = True
+    model = Admin
+    fields = ('email',)
+    queryset = Admin.objects.all()
+    success_url = reverse_lazy('admin_list')
+    success_message = 'Admin update successfully'
+    template_name = 'admins/create.html'
+
+
+@login_required()
+@permission_required('admins.delete_admin', raise_exception=True)
+def remove_admin(request, pk):
+    admin = get_object_or_404(Admin, pk=pk)
+    admin.delete()
+    messages.success(request, 'Admin removed successfully')
+    return redirect('admin_list')
