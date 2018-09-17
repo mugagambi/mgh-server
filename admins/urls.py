10ec43351b6462b730533d17881fe6b672cdf4ab
mugagambi <mugagambi@gmail.com>
2018-09-17 19:01:43 +0300

    add admins

--- admins/urls.py
+++ admins/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.AdminsList.as_view(), name='admin_list')
+    path('', views.AdminsList.as_view(), name='admin_list'),
+    path('add/', views.CreateAdmin.as_view(), name='add_admin')
 ]
--- admins/views.py
+++ admins/views.py
@@ -1,7 +1,25 @@
-from django.views.generic import ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (AccessMixin, LoginRequiredMixin,
+                                        PermissionRequiredMixin)
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
+
 from .models import Admin
+
 # Create your views here.
+
+
 class AdminsList(LoginRequiredMixin, ListView):
     model = Admin
     template_name = 'admins/list.html'
+
+
+class CreateAdmin(LoginRequiredMixin, PermissionRequiredMixin, AccessMixin, SuccessMessageMixin, CreateView):
+    permission_denied_message = 'You don\'t have access to this page.'
+    permission_required = 'admins.add_admin'
+    raise_exception = True
+    model = Admin
+    fields = ('email',)
+    success_url = reverse_lazy('admin_list')
+    success_message = 'Admin added successfully'
+    template_name = 'admins/create.html'
