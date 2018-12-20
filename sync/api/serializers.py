16f9c51382ec6d76f3d1457f81709da4581f5baf
Humphrey Mugambi <mugagambi@gmail.com>
2018-12-20 13:05:27 +0300

    packages serializer

--- sync/api/serializers.py
+++ sync/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from sales.models import Customer, Order, OrderProduct
+from sales.models import Customer, Order, OrderProduct, Package, PackageProduct
 
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -51,9 +51,40 @@ class OrderSerializer(serializers.ModelSerializer):
         return super(OrderSerializer, self).update(instance, validated_data)
 
 
+class PackageItemsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PackageProduct
+        fields = ('number', 'package', 'order_product', 'qty_weigh', 'crate_weight')
+
+    def create(self, validated_data):
+        return super(PackageItemsSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        return super(PackageItemsSerializer, self).update(instance, validated_data)
+
+
+class PackageSerializer(serializers.ModelSerializer):
+    packaged_by = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    items = PackageItemsSerializer(many=True, required=True)
+
+    class Meta:
+        model = Package
+        fields = ('order', 'number', 'packaged_by', 'items')
+
+    def create(self, validated_data):
+        return super(PackageSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        return super(PackageSerializer, self).update(instance, validated_data)
+
+
 class DataSerializer(serializers.Serializer):
     customers = CustomerSerializer(many=True, required=True)
     orders = OrderSerializer(many=True, required=True)
+    packages = PackageSerializer(many=True, required=True)
 
     def create(self, validated_data):
         return super(DataSerializer, self).create(validated_data)
