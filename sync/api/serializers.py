13001bdd1372b87f5b7a2b951676691293984c31
Humphrey Mugambi <mugagambi@gmail.com>
2018-12-20 10:23:33 +0300

    add orders serializer

--- sync/api/serializers.py
+++ sync/api/serializers.py
@@ -16,8 +16,21 @@ class CustomerSerializer(serializers.Serializer):
         return super(CustomerSerializer, self).update(instance, validated_data)
 
 
+class OrderSerializer(serializers.Serializer):
+    number = serializers.CharField(max_length=10, required=True)
+    customer = serializers.CharField(max_length=10, required=True)
+    date_delivery = serializers.DateField(required=True)
+
+    def create(self, validated_data):
+        return super(OrderSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        return super(OrderSerializer, self).update(instance, validated_data)
+
+
 class DataSerializer(serializers.Serializer):
-    customers = CustomerSerializer(many=True)
+    customers = CustomerSerializer(many=True, required=True)
+    orders = OrderSerializer(many=True, required=True)
 
     def create(self, validated_data):
         return super(DataSerializer, self).create(validated_data)
